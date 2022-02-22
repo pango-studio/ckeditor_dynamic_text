4a409b73a2e903e605cb7bceed54cf7337557f46
Jason Tame <jason@advantagelearn.com>
2022-02-22 09:39:08 +0200

    Update all CK editor depencies to latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,60 @@
 // webpack.config.js
 
-'use strict';
+'use strict'
 
-const path = require( 'path' );
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const path = require('path')
+const { styles } = require('@ckeditor/ckeditor5-dev-utils')
 
 module.exports = {
-    // https://webpack.js.org/configuration/entry-context/
-    entry: './samples/app.js',
+	// https://webpack.js.org/configuration/entry-context/
+	entry: './samples/app.js',
 
-    // https://webpack.js.org/configuration/output/
-    output: {
-        path: path.resolve( __dirname, 'dist' ),
-        filename: 'bundle.js'
-    },
+	// https://webpack.js.org/configuration/output/
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
 
-    module: {
-        rules: [
-            {
+	module: {
+		rules: [
+			{
 				test: /\.svg$/,
+				use: ['raw-loader'],
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: {
+							injectType: 'singletonStyleTag',
+							attributes: {
+								'data-cke': true,
+							},
+						},
+					},
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: styles.getPostCssConfig({
+								themeImporter: {
+									themePath: require.resolve(
+										'@ckeditor/ckeditor5-theme-lark'
+									),
+								},
+								minify: true,
+							}),
+						},
+					},
+				],
+			},
+		],
+	},
 
-                use: [ 'raw-loader' ]
-            },
-            {
-                test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+	// Useful for debugging.
+	devtool: 'source-map',
 
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            injectType: 'singletonStyleTag',
-                            attributes: {
-                                'data-cke': true
-                            }
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: styles.getPostCssConfig( {
-                            themeImporter: {
-                                themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-                            },
-                            minify: true
-                        } )
-                    }
-                ]
-            }
-        ]
-    },
-
-    // Useful for debugging.
-    devtool: 'source-map',
-
-    // By default webpack logs warnings if the bundle is bigger than 200kb.
-    performance: { hints: false }
-};
+	// By default webpack logs warnings if the bundle is bigger than 200kb.
+	performance: { hints: false },
+}
